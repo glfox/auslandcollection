291ca7826369a6808d8d47268afdffd7e6776525
glfox <qz@192.168.0.22>
2018-02-27 13:53:02 -0800

    update port number

--- auslandweixin/src/main/java/com/ausland/weixin/config/AuslandweixinConfig.java
+++ auslandweixin/src/main/java/com/ausland/weixin/config/AuslandweixinConfig.java
@@ -1,10 +1,17 @@
 package com.ausland.weixin.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 public class AuslandweixinConfig {
 
+	@Bean
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate;
+	}
 }
