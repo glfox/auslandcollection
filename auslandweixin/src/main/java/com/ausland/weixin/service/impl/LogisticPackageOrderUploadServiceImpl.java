75250f523f5022e39acec5244331bcf64765b06b
LLuo2 <LLuo2@WABELHLP0698929.gsm1900.org>
2018-02-28 01:08:45 -0800

    f

--- auslandweixin/src/main/java/com/ausland/weixin/service/impl/LogisticPackageOrderUploadServiceImpl.java
+++ auslandweixin/src/main/java/com/ausland/weixin/service/impl/LogisticPackageOrderUploadServiceImpl.java
@@ -1,9 +1,11 @@
 package com.ausland.weixin.service.impl;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ausland.weixin.service.LogisticPackageOrderUploadService;
 
+@Service
 public class LogisticPackageOrderUploadServiceImpl implements LogisticPackageOrderUploadService{
 
 	@Override
