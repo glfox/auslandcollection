f2b2837552b228dc83b25409268641cfd3c2472a
LLuo2 <LLuo2@WABELHLP0698929.gsm1900.org>
2018-03-03 08:16:53 -0800

    d

---
+++ auslandweixin/src/main/java/com/ausland/weixin/model/wsdl/zhonghuan/LogisticsServiceImplServiceSoapBindingImpl.java
@@ -0,0 +1,23 @@
+/**
+ * LogisticsServiceImplServiceSoapBindingImpl.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.ausland.weixin.model.wsdl.zhonghuan;
+
+public class LogisticsServiceImplServiceSoapBindingImpl implements com.ausland.weixin.model.wsdl.zhonghuan.LogisticsServiceI{
+    public java.lang.String getLogisticsInformation(java.lang.String fydh, java.lang.String countrytype) throws java.rmi.RemoteException, com.ausland.weixin.model.wsdl.zhonghuan.InvocationTargetException, com.ausland.weixin.model.wsdl.zhonghuan.NoSuchMethodException, com.ausland.weixin.model.wsdl.zhonghuan.IllegalAccessException {
+        return null;
+    }
+
+    public java.lang.String getTeltoFydh(java.lang.String chrsjrdh, java.lang.String username, java.lang.String password, java.lang.String didbh) throws java.rmi.RemoteException {
+        return null;
+    }
+
+    public java.lang.String getYtLoggistics(java.lang.String fydh) throws java.rmi.RemoteException, com.ausland.weixin.model.wsdl.zhonghuan.InvocationTargetException, com.ausland.weixin.model.wsdl.zhonghuan.NoSuchMethodException, com.ausland.weixin.model.wsdl.zhonghuan.IllegalAccessException {
+        return null;
+    }
+
+}
