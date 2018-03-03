ca8490f6ba9bf0e6767106d0514ad5fb9fa3e6e8
LLuo2 <LLuo2@WABELHLP0698929.gsm1900.org>
2018-03-03 13:44:48 -0800

    d

--- auslandweixin/src/main/java/com/ausland/weixin/model/reqres/QueryStockRes.java
+++ auslandweixin/src/main/java/com/ausland/weixin/model/reqres/QueryStockRes.java
@@ -1,5 +1,11 @@
 package com.ausland.weixin.model.reqres;
 
+ 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class QueryStockRes {
 
+	String errorDetails;
+	
 }
