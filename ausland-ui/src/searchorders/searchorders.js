f6dd6c4a4eaec2eb49d38c03257b9ebde1ab71e1
glfox <qz@10.239.61.242>
2018-03-11 01:41:25 -0800

    update UI

--- ausland-ui/src/searchorders/searchorders.js
+++ ausland-ui/src/searchorders/searchorders.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './searchorders.css';
+import OrderDetails from './orderdetails.js'
 
 class SearchOrders extends Component {
 
@@ -7,69 +7,25 @@ class SearchOrders extends Component {
 	    super(props);
 	    this.state = {
 	    	trackingNo: "",
-	    	isChanged: false,
-	    	details: null,
-	    	error: ""
+	    	details: null
 	    };
 	    this.handleChange = this.handleChange.bind(this);
 	    this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
 	handleChange(event) {
-		if (this.state.trackingNo !== event.target.value) {
-			this.setState({
-				isChanged: true,
-				trackingNo: event.target.value
-			});
-		} else {
-			this.setState({
-				isChanged: true,
-			});
-		}
+		this.setState({
+			trackingNo: event.target.value
+		});
 	}
 	handleSubmit(event) {
-		if (this.state.trackingNo > 0 && this.state.isChanged) {
-			this.getDetails(this.state.trackingNo);
+		if (this.state.trackingNo) {
+			console.log(this.state.trackingNo);
+			this.setState({
+				details: <OrderDetails orderNo={this.state.trackingNo} />
+			})
 		}
 		event.preventDefault();
-	}
-
-	getDetails(trackingNo) {
-		fetch("http://118.24.75.119/query/zhonghuan/detailsbytrackingno?trackingno=" + trackingNo)
-	      	.then(res => res.json())
-	      	.then(result => {
-	      		console.log(result);
-	      		if (result.back && result.back.logisticsback) {
-	      			let paklog = result.back.logisticsback;
-					let rows = [];
-					if (paklog.length > 0) {
-						for (let i = 0; i < paklog.length; i++) {
-							let log = paklog[i];
-							rows.push(
-								<tr key={log.ztai.toString()}>
-									<td>{log.time}</td>
-									<td>{log.ztai}</td>
-								</tr>
-							);
-						}
-						this.setState({
-							details: rows,
-							error: null
-						});
-					}
-	      		} else {
-					this.setState({
-						details: null,
-						error: result.errorDetails
-					});
-				}
-	        },
-	        // Note: it's important to handle errors here
-	        // instead of a catch() block so that we don't swallow
-	        // exceptions from actual bugs in components.
-	        (error) => {
-	     		console.log(error);
-	        })
 	}
 
 	render() {
@@ -83,14 +39,7 @@ class SearchOrders extends Component {
 			        <input type="submit" value="查询" />
 		      	</form>
 		      	<br/>
-		      	<div>
-		      		<table align="center">
-		      			<tbody>
-		      				{this.state.details}
-		      			</tbody>
-		      		</table>
-		      	</div>
-		      	<div>{this.state.error}</div>
+		      	{this.state.details}
 			</div>
 		)
 	}
