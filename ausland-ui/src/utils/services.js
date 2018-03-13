5f7a5303590191831f356c81ce6129ea91653cd4
cathy <cathy@Lan-Luo-Levnovo-T460s.mshome.net>
2018-03-13 14:29:45 -0700

    d

--- ausland-ui/src/utils/services.js
+++ ausland-ui/src/utils/services.js
@@ -1,8 +1,9 @@
-const BASE_URL = 'http://118.24.75.119';
-
+//const BASE_URL = 'http://118.24.75.119';
+const BASE_URL = 'http://localhost:8081';
+const CONTEXT_PATH = '/auslandtest';
 export { getOrderDetails };
 
 function getOrderDetails(trackingNo) {
-	let url = BASE_URL + '/auslandtest/query/zhonghuan/detailsbytrackingno?trackingno=' + trackingNo;
+	let url = BASE_URL + CONTEXT_PATH + '/query/zhonghuan/detailsbytrackingno?trackingno=' + trackingNo;
 	return fetch(url).then(res => res.json());
 }
