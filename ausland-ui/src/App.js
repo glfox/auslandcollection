7ffe803bb7c26df78dc788aa6ec11114c1ddd968
glfox <qz@192.168.0.22>
2018-03-14 18:59:41 -0700

    update bar

--- ausland-ui/src/App.js
+++ ausland-ui/src/App.js
@@ -6,9 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Ausland<br/>Collection</h1>
-        </header>
         <AuslandNavBar />
       </div>
     );
--- ausland-ui/src/layout/auslandbar.js
+++ ausland-ui/src/layout/auslandbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem} from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import "./auslandbar.css";
 
 class AuslandNavBar extends Component {
 
@@ -8,31 +9,31 @@ class AuslandNavBar extends Component {
       <Navbar collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
-            <a href="#brand">React-Bootstrap</a>
+            <a href="#brand">Ausland Collection</a>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav>
-            <NavItem eventKey={1} href="#">
-              Link 1
-            </NavItem>
-            <NavItem eventKey={2} href="#">
-              Link 2
-            </NavItem>
-            <NavItem eventKey={3} href="#">
-              Link 3
-            </NavItem>
-            <NavItem eventKey={4} href="#">
-              Link 4
-            </NavItem>
-          </Nav>
-          <Nav pullRight>
-            <NavItem eventKey={5} href="#">
-              登陆
-            </NavItem>
-          </Nav>
-        </Navbar.Collapse>
+          <Navbar.Collapse>
+            <Nav pullLeft>
+              <NavItem eventKey={1} href="#">
+                Link 1
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Link 2
+              </NavItem>
+              <NavItem eventKey={3} href="#">
+                Link 3
+              </NavItem>
+              <NavItem eventKey={4} href="#">
+                Link 4
+              </NavItem>
+            </Nav>
+            <Nav pullRight>
+              <NavItem eventKey={5} href="#">
+                登陆
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
       </Navbar>
     );
   }
